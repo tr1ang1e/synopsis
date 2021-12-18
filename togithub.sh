fd3b77f9d799be6eec8ea0bab5cb14eeb90fe2b5
Daniil <tr1ang1e.goog1e@gmail.com>
2021-12-19 00:53:28 +0300

    x

--- togithub.sh
+++ togithub.sh
@@ -3,26 +3,33 @@
 from=/media/sf_COMMON_coding/__synopsis/   # copy from path
 to=./                                      # copy to path
 mask=*                                     # filename mask
+except='~'                                 # prevent copying of opened files
 
-lastcommit=`git log --date=format:"%Y%m%d%H" --pretty=format:"%ad" | sed -n 1p`
-except='~'  # prevent copying of opened files
+# 'sed' so we have only the last commit
+lastcommit=`git log --date=format:"%Y%m%d%H%M" --pretty=format:"%ad" | sed -n 1p`
 
 # copy files were changed till last commit
 for name in $from$mask
 do
-  lastchange=`date -r "$name" +%Y%m%d%H`
+  lastchange=`date -r "$name" +%Y%m%d%H%M`
   newname="`basename "$name"`"
   if [ ${newname:0:1} != $except ] && [ $lastchange -gt $lastcommit ] 
   then
-    cp "$name" "$to$newname"
-    git add "$to$newname"
+    cp "$name" "$to$newname"    # copy
+    git add "$to$newname"       # immediately add to git
+    echo + "`basename "$name"`"  
   fi
 done
+
+if [ `date -r $0 +%Y%m%d%H%M` -gt $lastcommit ]
+then
+  git add $0
+  echo + $0
+fi
 
 ch=`git status | grep modified`
 if [ "$ch" ]
 then
-  git add $0
   git commit -m "x"
   git push
 fi
